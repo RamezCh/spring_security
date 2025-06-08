0dc04b241cf11b687dfc5fa496f9ec451598ca28
Ramez Chreide <ramiz_chreide@outlook.com>
2025-06-08 22:41:21 +0200

    add: basic security

--- src/main/java/com/github/ramezch/spring_security/controller/WelcomeController.java
+++ src/main/java/com/github/ramezch/spring_security/controller/WelcomeController.java
@@ -1,4 +1,13 @@
 package com.github.ramezch.spring_security.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class WelcomeController {
+
+    @GetMapping("/welcome")
+    public String sayWelcome() {
+        return "Welcome to Spring Application with Security";
+    }
 }
